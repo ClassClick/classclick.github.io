47ef101b9ed3300e2d1157d387b253f6d94acefc
Daan Breur <git@daanbreur.systems>
2024-01-25 12:15:11 +0100

    yay annalytis

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,17 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.className}>{children}</body>
+			<Script
+				async
+				src="https://www.googletagmanager.com/gtag/js?id=G-W9R8J55TPG"
+			></Script>
+			<Script id="gtag">
+				{`window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+
+							gtag('config', 'G-W9R8J55TPG');`}
+			</Script>
 		</html>
 	);
 }
